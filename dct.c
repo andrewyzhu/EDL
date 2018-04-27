af05c41183a3d39c74a88201ce76ba4e4c251971
Andrew Zhu <36798911+andrewyzhu@users.noreply.github.com>
2018-04-26 18:41:15 -0600

    Add files via upload

---
+++ dct.c
@@ -0,0 +1,24 @@
+/*
+ * dct.c
+ *
+ *  Created on: Apr 26, 2018
+ *      Author: anzh1
+ */
+
+
+#include "dct.h"
+
+void dct(float * mel, float * dct_result, float * mfcc){
+    int i;
+    int j;
+    for(i = 0; i < 26; i++){
+        dct_result[i] = 0;
+        for(j = 0; j < 26; j++){
+            dct_result[i] += mel[j]*(float)(cos((double)(3.14/26*(j+0.5)*i)));
+        }
+    }
+    int k;
+    for(k = 0; k < 12; k++){
+        mfcc[k] = dct_result[k+1];
+    }
+}
---
+++ dct.h
@@ -0,0 +1,19 @@
+/*
+ * dct.h
+ *
+ *  Created on: Apr 26, 2018
+ *      Author: anzh1
+ */
+#include "msp.h"
+#include <math.h>
+
+#ifndef DCT_H_
+#define DCT_H_
+
+void dct(float * mel, float * dct_result, float * mfcc);
+
+
+
+
+
+#endif /* DCT_H_ */
