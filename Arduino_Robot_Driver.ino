0502368b41b38bc89b8dead7f7488ac67118cad4
Andrew Hsu <37677217+Andyhsu69@users.noreply.github.com>
2018-04-26 18:11:09 -0600

    Add files via upload

--- Arduino_Robot_Driver.ino
+++ Arduino_Robot_Driver.ino
@@ -1,25 +1,26 @@
+
+
 // define pins
-const int pinON = 6;         // connect pin 6 to ON/OFF switch via 1k resistor, active HIGH
-volatile int enc_count_Left = 0;// initialize global counters to 0
-volatile int enc_count_Right = 0;
+const int pinON = 8;         // connect pin 6 to ON/OFF switch, active HIGH
 const int pinCW_Left = 7;    // connect pin 7 to clock-wise PMOS gate
-const int pinCC_Left = 8;    // connect pin 8 to counter-clock-wise PMOS gate
-const int pinSpeed_Left = 9; // connect pin 9 to speed reference
-const int pinCW_Right = 10;    // connect pin 10 to clock-wise PMOS gate
-const int pinCC_Right = 11;    // connect pin 11 to counter-clock-wise PMOS gate
-const int pinSpeed_Right = 12; // connect pin 12 to speed reference
+const int pinCC_Left = 6;    // connect pin 8 to counter-clock-wise PMOS gate
+const int pinCW_Right = 4;
+const int pinCC_Right = 9;
+const int pinSpeed_Left = 11; // connect pin 9 to speed reference
+const int pinSpeed_Right = 10;
+const int wheelspeed = 50;
 
+// encoder counter variable
+volatile int enc_count_Left = 0; // "volatile" means the variable is stored in RAM
+volatile int enc_count_Right = 0;
+
+// setup pins and initial values
 void setup() {
-  Serial.begin(9600);       // start serial communication via USB at 9600 bits per second (baud)
-  //pinMode(pinON, INPUT);    // set on/off switch pin as input
-
-  attachInterrupt(2, count_Left, RISING);
-  attachInterrupt(3, count_Right, RISING);
-
+  Serial.begin(9600);
   pinMode(pinON,INPUT);
   pinMode(pinCW_Left,OUTPUT);
   pinMode(pinCC_Left,OUTPUT);
-  pinMode(pinSpeed_Right,OUTPUT);
+  pinMode(pinSpeed_Left,OUTPUT);
   pinMode(pinCW_Right,OUTPUT);
   pinMode(pinCC_Right,OUTPUT);
   pinMode(pinSpeed_Right,OUTPUT);
@@ -29,10 +30,27 @@ void setup() {
   digitalWrite(pinCC_Left,LOW);   // stop counter-clockwise
   digitalWrite(pinCW_Right,LOW);   // stop clockwise
   digitalWrite(pinCC_Right,LOW);   // stop counter-clockwise
-  analogWrite(pinSpeed_Left,50);  // set speed reference, duty-cycle = 50/255
-  analogWrite(pinSpeed_Right,50);  // set speed reference, duty-cycle = 50/255
+  analogWrite(pinSpeed_Left,wheelspeed);  // set speed reference, duty-cycle = 50/255
+  analogWrite(pinSpeed_Right,wheelspeed);
+  /* 
+    Connect left-wheel encoder output to pin 2 (external Interrupt 0) via a 1k resistor
+    Rising edge of the encoder signal triggers an interrupt 
+    count_Left is the interrupt service routine attached to Interrupt 0 (pin 2)
+  */
+  attachInterrupt(0, count_Left, RISING);
+  attachInterrupt(1,count_Right,RISING);
+
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
 }
 
+/*
+  Interrupt 0 service routine
+  Increment enc_count_Left on each rising edge of the 
+  encoder signal connected to pin 2
+*/ 
 void count_Left(){
   enc_count_Left++;
 }
@@ -42,22 +60,90 @@ void count_Right(){
 }
 
 void loop() {
-  enc_count_Left = 0;
-  enc_count_Right = 0;
-//  int onoff_switch = digitalRead(pinON);   // read switch on/off state
-//  Serial.println(onoff_switch);            // send value as ASCII-encoded decimal 
-int Diff = abs(enc_count_Left - enc_count_Right);
-Serial.print("LEFT: ");
-Serial.println(enc_count_Left);
-Serial.print("RIGHT: ");
-Serial.println(enc_count_Right);
-Serial.print("DIFF ");
-Serial.println(Diff);
-digitalWrite(pinCW_Left,HIGH);            // go clockwise
-digitalWrite(pinCW_Right,HIGH);            // go clockwise
-  do {} while ((enc_count_Left < 3*12*64) && (enc_count_Right < 3*12*64));   // do nothing until wheel makes 3 rotations
+//  do {
+//    enc_count_Left = 0;                     // reset encoder counter to 0
+//    enc_count_Right = 0;
+//  } while (digitalRead(pinON) == LOW);      // wait for ON switch
+
+  //forward 1 meter
+
+  if(digitalRead(A0) == HIGH)
+  {
+  delay(1000);
+  digitalWrite(13,HIGH);                    // turn LED on
+  digitalWrite(pinCW_Left,HIGH);            // go clockwise
+  digitalWrite(pinCW_Right,HIGH);
+  digitalWrite(pinCC_Left, LOW);
+  digitalWrite(pinCC_Right, LOW);
+  do {} while ((enc_count_Left < (900-.45*wheelspeed)) && (enc_count_Right < (900-.45*wheelspeed)));   // do nothing until wheel makes 3 rotations (original value 1894 - .45*wheelspeed)
   digitalWrite(pinCW_Left,LOW);             // stop
   digitalWrite(pinCW_Right,LOW);
-  delay(500);                              // wait 0.5 seconds
+  digitalWrite(13,LOW);                     // turn LED off
+  delay(1000);                              // wait 1 second
+
+  enc_count_Left = 0;                     // reset encoder counter to 0
+  enc_count_Right = 0;
+  }
+
+if(digitalRead(A1) == HIGH)
+{
+  // left 90 degrees
+  delay(1000);
+  digitalWrite(13,HIGH);                    // turn LED on
+  digitalWrite(pinCW_Right,HIGH);            // go clockwise
+  digitalWrite(pinCC_Left,HIGH);
+  digitalWrite(pinCC_Right, LOW);
+  digitalWrite(pinCW_Left, LOW);
+  do {} while ((enc_count_Left < (500-.22*wheelspeed)) && (enc_count_Right < (500-.22*wheelspeed)));   // 90 degrees left
+  digitalWrite(pinCW_Right,LOW);             // stop
+  digitalWrite(pinCC_Left,LOW);
+  digitalWrite(13,LOW);                     // turn LED off
+  delay(1000);                              // wait 1 second
+
+  enc_count_Left=0;
+  enc_count_Right=0;
+}
+  
+
+  
+if(digitalRead(A2) == HIGH)
+{
+// right 90 degrees
+delay(1000);
+  digitalWrite(13,HIGH);                    // turn LED on
+  digitalWrite(pinCW_Left,HIGH);            // go clockwise
+  digitalWrite(pinCC_Right,HIGH);
+  digitalWrite(pinCC_Left, LOW);
+  digitalWrite(pinCW_Right, LOW);
+  do {} while ((enc_count_Left < (500-.22*wheelspeed)) && (enc_count_Right < (500-.22*wheelspeed)));   // 90 degrees left
+  digitalWrite(pinCW_Left,LOW);             // stop
+  digitalWrite(pinCC_Right,LOW);
+  digitalWrite(13,LOW);                     // turn LED off
+  delay(1000);                              // wait 1 second
+
+  enc_count_Left=0;
+  enc_count_Right=0;
 }
 
+if(digitalRead(A3) == HIGH)
+{
+  // reverse
+  delay(1000);
+  digitalWrite(13,HIGH);                    // turn LED on
+  digitalWrite(pinCC_Left,HIGH);            
+  digitalWrite(pinCC_Right,HIGH);
+  digitalWrite(pinCW_Left, LOW);
+  digitalWrite(pinCW_Right, LOW);
+  do {} while ((enc_count_Left < (900-.22*wheelspeed)) && (enc_count_Right < (900-.22*wheelspeed)));   
+  digitalWrite(pinCC_Left,LOW);             // stop
+  digitalWrite(pinCC_Right,LOW);
+  digitalWrite(13,LOW);                     // turn LED off
+  delay(1000);                              // wait 1 second
+
+  enc_count_Left=0;
+  enc_count_Right=0;
+
+}
+
+
+}
